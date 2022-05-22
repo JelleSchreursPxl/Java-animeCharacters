f543a28a131111c9f9ca6d0114154ba526b4e987
jelle.schreurs <jelle.schreurs@student.pxl.be>
2022-05-22 15:57:35 +0200

    Writing the first of MockMvc Tests

--- src/test/java/be/pxl/anime/builder/AnimeCharacterDetailDTOBuilder.java
+++ src/test/java/be/pxl/anime/builder/AnimeCharacterDetailDTOBuilder.java
@@ -1,7 +1,55 @@
 package be.pxl.anime.builder;
 
 import be.pxl.anime.api.anime_character.AnimeCharacterDTO;
+import be.pxl.anime.api.anime_character.AnimeCharacterDetailDTO;
+
+import java.util.Collections;
 
 public class AnimeCharacterDetailDTOBuilder extends AnimeCharacterDTO {
+    public static final Long ID = 14L;
+    public static final String FIRSTNAME = "Kakarot";
+    public static final String LASTNAME = "Bardock";
+    public static final String CHARACTER_NAME = "Son Goku";
+
+    private Long id = ID;
+    private String firstName = FIRSTNAME;
+    private String lastName = LASTNAME;
+    private String characterName = CHARACTER_NAME;
+
+    private AnimeCharacterDetailDTOBuilder(){
+    }
+
+    public static AnimeCharacterDetailDTOBuilder aAnimeCharacterDetailDTO() { return new AnimeCharacterDetailDTOBuilder(); }
+
+    public AnimeCharacterDetailDTOBuilder withId(Long id){
+        this.id = id;
+        return this;
+    }
+
+    public AnimeCharacterDetailDTOBuilder withfirstName(String firstName){
+        this.firstName = firstName;
+        return this;
+    }
+
+    public AnimeCharacterDetailDTOBuilder withlastName(String lastName){
+        this.lastName = lastName;
+        return this;
+    }
+
+    public AnimeCharacterDetailDTOBuilder withCharacterName(String characterName){
+        this.characterName = characterName;
+        return this;
+    }
+
+    public AnimeCharacterDetailDTO build(){
+        AnimeCharacterDetailDTO animeCharacter = new AnimeCharacterDetailDTO();
+        animeCharacter.setId(id);
+        animeCharacter.setFirstName(firstName);
+        animeCharacter.setLastName(lastName);
+        animeCharacter.setAnimeCharacterName(characterName);
+        animeCharacter.setMissions(Collections.emptyList());
+        return animeCharacter;
+    }
+
 
 }
--- src/test/java/be/pxl/anime/mocking/AnimeCharacterControllerGetAnimeCharacterTest.java
+++ src/test/java/be/pxl/anime/mocking/AnimeCharacterControllerGetAnimeCharacterTest.java
@@ -1,13 +1,26 @@
 package be.pxl.anime.mocking;
 
+import be.pxl.anime.api.anime_character.AnimeCharacterDetailDTO;
+import be.pxl.anime.builder.AnimeCharacterDetailDTOBuilder;
+import be.pxl.anime.exception.ResourceNotFoundException;
+import be.pxl.anime.rest.AnimeCharacterController;
 import be.pxl.anime.service.AnimeCharacterService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-@WebMvcTest
+import static be.pxl.anime.builder.AnimeCharacterDTOBuilder.*;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = AnimeCharacterController.class)
 public class AnimeCharacterControllerGetAnimeCharacterTest {
 
     @MockBean
@@ -18,6 +31,29 @@ public class AnimeCharacterControllerGetAnimeCharacterTest {
 
     @Test
     public void getAnimeCharacterById_ReturnsExistingAnimeCharacter() throws Exception {
+        AnimeCharacterDetailDTO animeCharacterDetailDTO =
+                AnimeCharacterDetailDTOBuilder
+                        .aAnimeCharacterDetailDTO()
+                        .withId(ANIMECHARACTER_ID)
+                        .build();
+        when(animeCharacterService.findAnimeCharacterById(ANIMECHARACTER_ID)).thenReturn(animeCharacterDetailDTO);
+        mockMvc.perform(MockMvcRequestBuilders.get("anime_character/{id}", ANIMECHARACTER_ID)
+                                              .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(ANIMECHARACTER_ID))
+                .andExpect(jsonPath("$.firstName").value(FIRSTNAME))
+                .andExpect(jsonPath("$.lastName").value(LASTNAME))
+                .andExpect(jsonPath("$.characterName").value(ANIMECHARACTER_NAME));
+    }
 
+    @Test
+    public void getAnimeCharacterById_ReturnsNotFoundWhenResourceNotFoundExceptionIsThrown() throws Exception {
+        doThrow(ResourceNotFoundException.class)
+                .when(animeCharacterService)
+                .findAnimeCharacterById(ANIMECHARACTER_ID);
+        mockMvc.perform(MockMvcRequestBuilders.get("anime_character/{id}", ANIMECHARACTER_ID)
+                                              .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
     }
 }
